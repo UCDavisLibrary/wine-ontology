e2e8dcf5ed26836079bc860d173b7195ec370071
Quinn Hart <qjhart@ucdavis.edu>
2016-10-13 11:08:19 -0700

    Added France Example

--- examples/vision/vision.sql
+++ examples/vision/vision.sql
@@ -12,19 +12,19 @@ CREATE TABLE labels (
 \COPY labels from Amerine_5.tsv with CSV DELIMITER e'\t' QUOTE e'\r' NULL '\N' ENCODING 'UTF-8'
 
 
-create view label_dates as 
+create view label_dates as
 with f as (
- select label,vision->'text'->0->'desc' as t from vision
+ select label,vision->'text'->0->'desc' as t from labels
 ), c as (
  select distinct label,
-  regexp_replace(unnest(regexp_matches(t::text,'((?:(?:1\s*[89])|2\s*0)\s*\d\s*\d)','g')),'\s','','g') as year 
+  regexp_replace(unnest(regexp_matches(t::text,'((?:(?:1\s*[89])|2\s*0)\s*\d\s*\d)','g')),'\s','','g') as year
   from f
 )
-select label,array_agg(year order by year) as years from c 
+select label,array_agg(year order by year) as years from c
 group by label;
 
 create view common_phrases as
 select lower(jsonb_array_elements(vision->'text')->>'desc') as phrase,
-count(*)  
-from labels 
+count(*)
+from labels
 group by 1 order by 2 desc;
